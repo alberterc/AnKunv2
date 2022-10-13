cca86600f2ce90d4ceef0f43dd9762cef61d5550
alberterc <85069947+alberterc@users.noreply.github.com>
2022-10-14 03:09:52 +0700

    Removed unnecessary imports.

--- app/src/main/java/com/radx/ankunv2/ui/theme/Theme.kt
+++ app/src/main/java/com/radx/ankunv2/ui/theme/Theme.kt
@@ -3,6 +3,7 @@ package com.radx.ankunv2.ui.theme
 import android.app.Activity
 import android.os.Build
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.shapes
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
@@ -12,7 +13,6 @@ import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
