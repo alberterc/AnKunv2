646ccaa15bdf695487b2b4156257a7b02b4761b3
alberterc <85069947+alberterc@users.noreply.github.com>
2022-10-15 23:39:05 +0700

    Added anime search class for searching and getting anime search list.

---
+++ app/src/main/java/com/radx/ankunv2/anime/AnimeSearch.kt
@@ -0,0 +1,48 @@
+package com.radx.ankunv2.anime
+
+import java.net.URL
+
+object AnimeSearch {
+    private var searchResultList: List<List<String>> = mutableListOf()
+
+    fun getSearchResultList(
+        search: String = "", season: String = "", genres: String = "",
+        dub: String = "", airing: String = "", sort: String = "popular-week",
+        page: String = "1"
+    ): List<List<String>> {
+        getSearchResult(
+            search = search, season = season, genres = genres,
+            dub = dub, airing = airing, sort = sort,
+            page = page
+        )
+        return this.searchResultList
+    }
+
+    private fun getSearchResult(
+        search: String = "", season: String = "", genres: String = "",
+        dub: String = "", airing: String = "", sort: String = "popular-week",
+        page: String = "1"
+    ) {
+         val searchUrl =
+            "/public-api" +
+                    "/search.php?search_text=" + search +
+                    "&season=" + season +
+                    "&genres=" + genres +
+                    "&dub=" + dub +
+                    "&airing=" + airing +
+                    "&sort=" + sort +
+                    "&page=" + page
+
+        // get anime search result data from JSON response
+        // retrieves JSON response in String data type
+        val animeSearchListJSONStr = URL(Utils.animeBaseUrl + searchUrl).readText()
+            .replace("\\", "")
+            .replace("[", "")
+            .replace("]", "")
+        // convert from String into List of List
+        // [[Anime Title, Anime ID, ANIME THUMBNAIL, SUB OR DUB (sub=0, dub=1]]
+        searchResultList = animeSearchListJSONStr
+            .split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*\$)".toRegex())
+            .chunked(4)
+    }
+}
