f1dcc61990dfb910b05ec215b6743de5f0478514
alberterc <85069947+alberterc@users.noreply.github.com>
2022-10-16 02:28:46 +0700

    Added a class to get a slider that shows the latest anime.

---
+++ app/src/main/java/com/radx/ankunv2/anime/AnimeSlider.kt
@@ -0,0 +1,28 @@
+package com.radx.ankunv2.anime
+
+import java.net.URL
+
+object AnimeSlider {
+    private var sliderList: List<List<String>> = mutableListOf()
+
+    fun getSliderResultList(): List<List<String>> {
+        getSliderResult()
+        return this.sliderList
+    }
+
+    private fun getSliderResult() {
+        val sliderUrl = "public-api/slider.php"
+
+        // get anime search result data from JSON response
+        // retrieves JSON response in String data type
+        val animeSliderListJSONStr = URL(Utils.animeBaseUrl + sliderUrl).readText()
+            .replace("\\", "")
+            .replace("[", "")
+            .replace("]", "")
+        // convert from String into List of List
+        // [[Anime Title, Anime ID, ANIME THUMBNAIL, SUB OR DUB (sub=0, dub=1]]
+        sliderList = animeSliderListJSONStr
+            .split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*\$)".toRegex())
+            .chunked(3)
+    }
+}
