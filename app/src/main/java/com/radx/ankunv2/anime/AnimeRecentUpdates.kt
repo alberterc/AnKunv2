338606bacdeaaaeb74b7cfef684b1b67918c44c3
alberterc <85069947+alberterc@users.noreply.github.com>
2022-10-16 23:58:08 +0700

    Added a class to get recent episode anime updates.

---
+++ app/src/main/java/com/radx/ankunv2/anime/AnimeRecentUpdates.kt
@@ -0,0 +1,30 @@
+package com.radx.ankunv2.anime
+
+import java.net.URL
+
+object AnimeRecentUpdates {
+    private var recentUpdatesList: List<List<String>> = mutableListOf()
+
+    fun getRecentUpdatesList(mode: String = "sub") : List<List<String>> {
+        getRecentUpdates(mode = mode)
+        return this.recentUpdatesList
+    }
+
+    private fun getRecentUpdates(page: String = "1", mode: String = "sub") {
+        val recentUpdatesUrl = "/public-api/index.php?" +
+                "page=" + page +
+                "&mode=" + mode
+
+        // get anime latest list data from JSON response
+        // retrieves JSON response in String data type
+        val animeRecentUpdatesJSONStr = URL(Utils.animeBaseUrl + recentUpdatesUrl).readText()
+            .replace("\\", "")
+            .replace("[", "")
+            .replace("]", "")
+        // convert from String into List of List
+        // [[Anime Title, Anime ID, UNKNOWN, TOTAL EP, ANIME THUMBNAIL, LAST EP RELEASE TIME]]
+        recentUpdatesList = animeRecentUpdatesJSONStr
+            .split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*\$)".toRegex())
+            .chunked(6)
+    }
+}
