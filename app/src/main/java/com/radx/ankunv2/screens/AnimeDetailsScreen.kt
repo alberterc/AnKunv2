097bd28a0bc1332aafb2dcd0ee5c962b97bf5442
alberterc <85069947+alberterc@users.noreply.github.com>
2022-10-19 00:18:27 +0700

    Added a gradient to the large thumbnail, changed favorite button to an icon, added list to show episodes.

--- app/src/main/java/com/radx/ankunv2/screens/AnimeDetailsScreen.kt
+++ app/src/main/java/com/radx/ankunv2/screens/AnimeDetailsScreen.kt
@@ -1,35 +1,30 @@
 package com.radx.ankunv2.screens
 
-import android.util.Log
 import androidx.compose.foundation.*
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
-import com.radx.ankunv2.R
 import com.radx.ankunv2.anime.AnimeDetails
+import com.radx.ankunv2.anime.Utils
 import com.radx.ankunv2.ui.theme.BrightGrey
 import com.radx.ankunv2.ui.theme.Grey
 import com.radx.ankunv2.ui.theme.Transparent
@@ -53,16 +48,21 @@ fun AnimeDetailsScreen(animeID: String) {
             "title" to "",
             "description" to "",
             "status" to "",
+            "episode count" to "",
             "type" to "",
             "season" to "",
             "small thumbnail" to "",
             "large thumbnail" to ""
         )) }
+        var animeEpisodesItemsState by remember { mutableStateOf(listOf(listOf(""))) }
+
         LaunchedEffect(Unit) {
             getGenreItemsList(animeID = animeID)
             genreItemsState = genreItems
             getAnimeDetailsMap(animeID = animeID)
             animeDetailsState = animeDetails
+            getAnimeEpisodeList(animeID = animeID)
+            animeEpisodesItemsState = episodesList
         }
 
         Top(
@@ -75,9 +75,10 @@ fun AnimeDetailsScreen(animeID: String) {
         Bottom(
             modifier = Modifier
                 .align(Alignment.BottomCenter)
-                .height(bottomHeight - 110.dp),
+                .height(bottomHeight - 75.dp),
             genreItemsState,
-            animeDetailsState
+            animeDetailsState,
+            animeEpisodesItemsState
         )
 
         Center(
@@ -95,7 +96,6 @@ fun AnimeDetailsScreen(animeID: String) {
 @Composable
 fun Top(modifier: Modifier, animeDetailsState: Map<String, String>) {
     val largeThumbnail = if (animeDetailsState["large thumbnail"] == null) "" else animeDetailsState["large thumbnail"]!!
-    Log.e("THUMB", largeThumbnail)
 
     Column(modifier.background(Color.Transparent)) {
         Card(
@@ -104,12 +104,28 @@ fun Top(modifier: Modifier, animeDetailsState: Map<String, String>) {
                 .fillMaxWidth(),
             shape = RoundedCornerShape(0.dp)
         ) {
-            Image(
-                painter = rememberAsyncImagePainter(largeThumbnail),
-                contentDescription = null,
-                modifier = Modifier.fillMaxHeight(),
-                contentScale = ContentScale.Crop
-            )
+            Box {
+                Image(
+                    painter = rememberAsyncImagePainter(largeThumbnail),
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxHeight(),
+                    contentScale = ContentScale.Crop
+                )
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(
+                            brush = Brush.verticalGradient(
+                                colors = listOf(
+                                    Transparent,
+                                    Color.Black
+                                ),
+                                startY = 0f,
+                                endY = 1000f
+                            )
+                        )
+                )
+            }
         }
     }
 }
@@ -134,21 +150,40 @@ fun Center(modifier: Modifier, animeDetailsState: Map<String, String>) {
             // small thumbnail
             Card(
                 modifier = Modifier
-                    .height(200.dp)
-                    .width(150.dp)
+                    .height(160.dp)
+                    .width(110.dp)
             ) {
-                Image(
-                    painter = rememberAsyncImagePainter(thumbnail),
-                    contentDescription = null,
-                    modifier = Modifier.fillMaxSize(),
-                    contentScale = ContentScale.FillBounds
-                )
+                Box {
+                    Image(
+                        painter = rememberAsyncImagePainter(thumbnail),
+                        contentDescription = null,
+                        modifier = Modifier.fillMaxSize(),
+                        contentScale = ContentScale.FillBounds
+                    )
+                    if (animeDetailsState["type"] == "Dubbed") {
+                        Card(
+                            modifier = Modifier
+                                .align(Alignment.TopEnd)
+                                .padding(0.dp, 8.dp, 8.dp, 0.dp),
+                            shape = RoundedCornerShape(18.dp)
+                        ) {
+                            Text(
+                                modifier = Modifier
+                                    .background(color = MaterialTheme.colorScheme.onSecondaryContainer)
+                                    .padding(5.dp, 2.dp, 5.dp, 2.dp),
+                                text = "DUB",
+                                fontSize = 14.sp,
+                                fontWeight = FontWeight.SemiBold
+                            )
+                        }
+                    }
+                }
             }
 
             // title, status, episodes, season, favorite button
             Column(
                 modifier = Modifier
-                    .padding(12.dp, 40.dp, 0.dp, 0.dp)
+                    .padding(12.dp, 0.dp, 0.dp, 0.dp)
             ) {
                 Text(
                     text = title,
@@ -162,7 +197,7 @@ fun Center(modifier: Modifier, animeDetailsState: Map<String, String>) {
                 Row {
                     Column(
                         modifier = Modifier
-                            .padding(0.dp, 10.dp, 0.dp, 0.dp)
+                            .padding(0.dp, 0.dp, 0.dp, 0.dp)
                     ) {
                         Text(
                             text = status,
@@ -172,7 +207,7 @@ fun Center(modifier: Modifier, animeDetailsState: Map<String, String>) {
                                 .padding(0.dp, 0.dp, 0.dp, 8.dp)
                         )
                         Text(
-                            text = "Episodes",
+                            text = animeDetailsState["episode count"] + " episodes",
                             fontSize = 12.sp,
                             color = BrightGrey,
                             modifier = Modifier
@@ -188,58 +223,39 @@ fun Center(modifier: Modifier, animeDetailsState: Map<String, String>) {
                     }
                     Column(
                         modifier = Modifier
-                            .padding(19.dp, 23.dp, 0.dp, 0.dp)
+                            .padding(0.dp, 0.dp, 0.dp, 0.dp)
                     ) {
 //                        val rating: Float by remember { mutableStateOf(0f) }
 
-                        if (!favoriteState) {
-                            Button(
-                                enabled =  true,
-                                onClick = { },
-                                colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.onSecondaryContainer),
-                                modifier = Modifier
-                                    .width(119.dp),
-                                content = {
-                                    Row(
-                                        horizontalArrangement = Arrangement.spacedBy(8.dp),
-                                        verticalAlignment = Alignment.CenterVertically,
-                                        modifier = Modifier
-                                            .padding(0.dp, 3.dp, 0.dp, 3.dp)
-                                            .fillMaxWidth()
-                                    ) {
-                                        Icon(
-                                            imageVector = Icons.Default.Add,
-                                            contentDescription = null,
-                                            modifier = Modifier
-                                                .width(12.dp)
-                                                .height(12.dp)
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            contentAlignment = Alignment.TopEnd
+                        ) {
+                            if (!favoriteState) {
+                                Icon(
+                                    imageVector = Icons.Outlined.FavoriteBorder,
+                                    contentDescription = null,
+                                    modifier = Modifier
+                                        .clickable(
+                                            interactionSource = remember { MutableInteractionSource() },
+                                            indication = rememberRipple(color = MaterialTheme.colorScheme.secondary),
+                                            onClick = { }
                                         )
-                                        Text(
-                                            text = "Favorites",
-                                            fontSize = 12.sp,
-                                            modifier = Modifier.padding(0.dp, 0.dp, 0.dp, 1.dp)
+                                )
+                            }
+                            else {
+                                Icon(
+                                    imageVector = Icons.Filled.Favorite,
+                                    contentDescription = null,
+                                    modifier = Modifier
+                                        .clickable(
+                                            interactionSource = remember { MutableInteractionSource() },
+                                            indication = rememberRipple(color = MaterialTheme.colorScheme.secondary),
+                                            onClick = { }
                                         )
-                                    }
-                                }
-                            )
-                        }
-                        else {
-                            OutlinedButton(
-                                enabled =  true,
-                                onClick = { },
-                                colors = ButtonDefaults.outlinedButtonColors(
-                                    contentColor = MaterialTheme.colorScheme.onSecondaryContainer
-                                ),
-                                border = BorderStroke(1.dp, MaterialTheme.colorScheme.onSecondaryContainer),
-                                content = {
-                                    Text(
-                                        text = "Favorited",
-                                        fontSize = 12.sp,
-                                        modifier = Modifier
-                                            .padding(0.dp, 0.dp, 0.dp, 0.dp),
-                                    )
-                                }
-                            )
+                                )
+                            }
                         }
 
                         // rating bar lib
@@ -254,16 +270,19 @@ fun Center(modifier: Modifier, animeDetailsState: Map<String, String>) {
 
 // anime details
 @Composable
-fun Bottom(modifier: Modifier, genreItemsState: List<String>, animeDetailsState: Map<String, String>) {
-    val episodeItemsState by remember { mutableStateOf(listOf("")) }
-
+fun Bottom(
+    modifier: Modifier,
+    genreItemsState: List<String>,
+    animeDetailsState: Map<String, String>,
+    animeEpisodesItemsState: List<List<String>>
+) {
     val description = if (animeDetailsState["description"] == null) "" else animeDetailsState["description"]!!
 
     Column(
         modifier
             .verticalScroll(rememberScrollState())
             .background(Color.Transparent)
-            .padding(16.dp, 8.dp, 16.dp, 0.dp)
+            .padding(16.dp, 0.dp, 16.dp, 0.dp)
             .fillMaxSize()
     ) {
         // genre
@@ -304,9 +323,11 @@ fun Bottom(modifier: Modifier, genreItemsState: List<String>, animeDetailsState:
                 .fillMaxWidth(),
             horizontalArrangement = Arrangement.spacedBy(16.dp),
         ) {
-            items(episodeItemsState) { item ->
+            items(animeEpisodesItemsState) { item ->
                 // each episode card
-                EpisodeCardItem()
+                if (item.size != 1) {
+                    EpisodeCardItem(item, animeEpisodesItemsState)
+                }
             }
         }
     }
@@ -337,19 +358,64 @@ fun GenreCardItem(genre: String) {
 }
 
 @Composable
-fun EpisodeCardItem() {
+fun EpisodeCardItem(episode: List<String>, animeEpisodesItemsState: List<List<String>>) {
+    // [[UNKNOWN, EPISODE ID, EPISODE NUMBER, EPISODE RELEASE TIME]]
+    val id = episode[1]
+    val num = episode[2]
+    val releaseTime: String
+
+    val seconds = Utils.getTimeFromEpoch(episode[3]) / 1000
+    val minutes = seconds / 60
+    val hours = minutes / 60
+    val days = hours / 24
+    // simple string formatting for episode release time
+    if (days > 1) {
+        releaseTime = "$days days ago"
+    }
+    else if (days > 0) {
+        releaseTime = "$days day ago"
+    }
+    else if (hours > 1) {
+        releaseTime = "$hours hrs. ago"
+    }
+    else if (hours > 0) {
+        releaseTime = "$hours hr. ago"
+    }
+    else if (minutes > 1) {
+        releaseTime = "$minutes mins. ago"
+    }
+    else if (minutes > 0) {
+        releaseTime = "$minutes min. ago"
+    }
+    else if (seconds > 1) {
+        releaseTime = "$seconds secs. ago"
+    }
+    else if (seconds > 0) {
+        releaseTime = "$seconds sec. ago"
+    }
+    else {
+        releaseTime = "Just Released"
+    }
+
     FilledTonalButton(
         enabled =  true,
         onClick = { },
         modifier = Modifier
             .width(130.dp),
         content = {
-            Text(
-                text = "Episode 1",
-                fontSize = 14.sp,
-                modifier = Modifier
-                    .padding(0.dp, 0.dp, 0.dp, 0.dp),
-            )
+            Column {
+                Text(
+                    text = "Episode " + num,
+                    fontSize = 14.sp,
+                    modifier = Modifier
+                        .padding(0.dp, 0.dp, 0.dp, 0.dp),
+                )
+                Text(
+                    text = releaseTime,
+                    fontSize = 12.sp,
+                    color = BrightGrey
+                )
+            }
         }
     )
 }
@@ -366,6 +432,7 @@ var animeDetails = mapOf(
     "title" to "",
     "description" to "",
     "status" to "",
+    "episode count" to "",
     "type" to "",
     "season" to "",
     "small thumbnail" to "",
@@ -376,4 +443,12 @@ suspend fun getAnimeDetailsMap(animeID: String) = withContext(Dispatchers.IO) {
 }
 fun fillAnimeDetailsMap(animeID: String) {
     animeDetails = AnimeDetails.getAnimeDetailsList(animeID = animeID)
+}
+
+var episodesList = listOf(listOf(""))
+suspend fun getAnimeEpisodeList(animeID: String) = withContext(Dispatchers.IO) {
+    fillAnimeEpisodeList(animeID = animeID)
+}
+fun fillAnimeEpisodeList(animeID: String) {
+    episodesList = AnimeDetails.getAnimeEpisodesList(animeID = animeID)
 }
