88d3efc56506f105c1b1ab6c338189dfeb38eb6c
alberterc <85069947+alberterc@users.noreply.github.com>
2022-10-18 22:21:31 +0700

    Added a function to get episode list and changed function names.

--- app/src/main/java/com/radx/ankunv2/anime/AnimeDetails.kt
+++ app/src/main/java/com/radx/ankunv2/anime/AnimeDetails.kt
@@ -1,22 +1,29 @@
 package com.radx.ankunv2.anime
 
 import org.jsoup.Jsoup
+import java.net.URL
 
 object AnimeDetails {
     private var animeDetailsMap: MutableMap<String, String> = mutableMapOf()
+    private var animeEpisodesList: List<List<String>> = mutableListOf()
     private var animeGenreList: List<String> = listOf("")
 
     fun getAnimeDetailsList(animeID: String): Map<String, String> {
-        getAnimeDetails(animeID = animeID)
+        animeDetailsResponse(animeID = animeID)
         return this.animeDetailsMap
     }
 
+    fun getAnimeEpisodesList(animeID: String): List<List<String>> {
+        animeEpisodesResponse(animeID = animeID)
+        return this.animeEpisodesList
+    }
+
     fun getAnimeGenreList(animeID: String): List<String> {
-        getAnimeGenre(animeID = animeID)
+        animeGenreResponse(animeID = animeID)
         return this.animeGenreList
     }
 
-    private fun getAnimeDetails(animeID: String) {
+    private fun animeDetailsResponse(animeID: String) {
         val detailsUrl = Utils.animeBaseUrl + animeID
 
         val webPage = Jsoup.connect(detailsUrl).get()
@@ -27,6 +34,9 @@ object AnimeDetails {
         detailsHTML.select("div.spe").select("span").eachText().forEach { item ->
             if (item.substringBefore(":").trim() == "Status") {
                 animeDetailsMap["status"] = item.substringAfter(":").trim()
+            }
+            else if (item.substringBefore(":").trim() == "Episodes") {
+                animeDetailsMap["episode count"] = item.substringAfter(":").trim()
             }
             else if (item.substringBefore(":").trim() == "Type") {
                 animeDetailsMap["type"] = item.substringAfter(":").trim()
@@ -44,7 +54,23 @@ object AnimeDetails {
             .trim()
     }
 
-    private fun getAnimeGenre(animeID: String) {
+    private fun animeEpisodesResponse(animeID: String) {
+        val episodesUrl = Utils.animeBaseUrl + "public-api/episodes.php?id=${animeID}"
+
+        // get anime episodes data from JSON response
+        // retrieves JSON response in String data type
+        val animeEpisodesJSONStr = URL(episodesUrl).readText()
+            .replace("\\", "")
+            .replace("[", "")
+            .replace("]", "")
+        // convert from String into List of List
+        // [[UNKNOWN, EPISODE ID, EPISODE NUMBER, EPISODE RELEASE TIME]]
+        animeEpisodesList = animeEpisodesJSONStr
+            .split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*\$)".toRegex())
+            .chunked(4)
+    }
+
+    private fun animeGenreResponse(animeID: String) {
         val detailsUrl = Utils.animeBaseUrl + animeID
 
         val webPage = Jsoup.connect(detailsUrl).get()
