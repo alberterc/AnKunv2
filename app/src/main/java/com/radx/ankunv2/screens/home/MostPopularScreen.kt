902045cfb32e5065c2fa006e8fe2c2e11e1cfdc2
alberterc <85069947+alberterc@users.noreply.github.com>
2022-10-18 18:37:35 +0700

    Added onclick function to show anime details.

--- app/src/main/java/com/radx/ankunv2/screens/home/MostPopularScreen.kt
+++ app/src/main/java/com/radx/ankunv2/screens/home/MostPopularScreen.kt
@@ -1,6 +1,5 @@
 package com.radx.ankunv2.screens.home
 
-import android.util.Log
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -24,26 +23,63 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import coil.compose.rememberAsyncImagePainter
 import com.radx.ankunv2.anime.AnimeDetails
+import com.radx.ankunv2.anime.AnimeDetailsScreenNav
 import com.radx.ankunv2.anime.AnimeSearch
+import com.radx.ankunv2.screens.AnimeDetailsScreen
 import com.radx.ankunv2.ui.theme.BrightGrey
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 @Composable
-fun MostPopularWeekScreen(navController: NavHostController) {
+fun MostPopularScreenNavigationHost(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = HomeMenus.PopularWeek.route
+    ) {
+        composable(
+            route = "${HomeMenus.PopularWeek.route}/{popularType}/{page}",
+            arguments = listOf(
+                navArgument("popularType") {
+                    type = NavType.StringType
+                },
+                navArgument("page") {
+                    type = NavType.StringType
+                })
+        ) {
+            MostPopularScreen(navController, it.arguments!!.getString("popularType")!!, it.arguments!!.getString("page")!!)
+        }
+        composable(
+            route = "${AnimeDetailsScreenNav.AnimeDetails.route}/{animeId}",
+            arguments = listOf(navArgument("animeId") {
+                type = NavType.StringType
+            })
+        ) {
+            AnimeDetailsScreen(it.arguments!!.getString("animeId")!!)
+        }
+    }
+}
+
+@Composable
+fun MostPopularScreen(navController: NavHostController, popularType: String, page: String) {
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp, 0.dp, 16.dp, 16.dp)
     ) {
         var mostPopularItemsState by remember { mutableStateOf(listOf(listOf(""))) }
-        LaunchedEffect(true) {
-            getMostPopularDetailedList(page = "1")
+        var currentPage = page
+        LaunchedEffect(Unit) {
+            getMostPopularDetailedList(page = currentPage, sort = popularType)
             mostPopularItemsState = mostPopularItems
         }
 
+        // top app bar
         Row(
             verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
@@ -60,12 +96,65 @@ fun MostPopularWeekScreen(navController: NavHostController) {
                         onClick = { navController.popBackStack() }
                     )
             )
+            if (popularType == "popular-week") {
+                Text(
+                    text = "Most Popular This Week",
+                    fontSize = 22.sp
+                )
+            }
+            else {
+                Text(
+                    text = "Most Popular This Year",
+                    fontSize = 22.sp
+                )
+            }
+        }
+
+        // next and previous page
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .padding(0.dp, 0.dp, 0.dp, 16.dp)
+                .align(Alignment.CenterHorizontally)
+        ) {
+            FilledTonalButton(
+                enabled = currentPage != "1",
+                modifier = Modifier.padding(0.dp, 0.dp, 40.dp, 0.dp),
+                content = { Text(text = "Prev") },
+                onClick = {
+                    if (popularType == "popular-week") {
+                        currentPage = (Integer.parseInt(currentPage) - 1).toString()
+                        navController.navigate("${HomeMenus.PopularWeek.route}/$popularType/$currentPage")
+                    }
+                    else {
+                        currentPage = (Integer.parseInt(currentPage) - 1).toString()
+                        navController.navigate("${HomeMenus.PopularWeek.route}/$popularType/$currentPage")
+                    }
+                }
+            )
             Text(
-                text = "Most Popular This Week",
-                fontSize = 22.sp
+                text = currentPage,
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 16.sp
+            )
+            FilledTonalButton(
+                enabled = true,
+                modifier = Modifier.padding(40.dp, 0.dp, 0.dp, 0.dp),
+                content = { Text(text = "Next") },
+                onClick = {
+                    if (popularType == "popular-week") {
+                        currentPage = (Integer.parseInt(currentPage) + 1).toString()
+                        navController.navigate("${HomeMenus.PopularYear.route}/$popularType/$currentPage")
+                    }
+                    else {
+                        currentPage = (Integer.parseInt(currentPage) + 1).toString()
+                        navController.navigate("${HomeMenus.PopularYear.route}/$popularType/$currentPage")
+                    }
+                }
             )
         }
 
+        // anime list
         LazyColumn(
             verticalArrangement = Arrangement.spacedBy(16.dp),
             modifier = Modifier
@@ -73,7 +162,7 @@ fun MostPopularWeekScreen(navController: NavHostController) {
         ) {
             items(mostPopularItemsState) {item ->
                 if (mostPopularItemsState.size != 1) {
-                    AnimeMostPopularDetailedCardItem(item)
+                    AnimeMostPopularDetailedCardItem(item, navController)
                 }
             }
         }
@@ -81,7 +170,7 @@ fun MostPopularWeekScreen(navController: NavHostController) {
 }
 
 @Composable
-fun AnimeMostPopularDetailedCardItem(anime: List<String>) {
+fun AnimeMostPopularDetailedCardItem(anime: List<String>, navController: NavHostController) {
     // [[Anime Title, Anime ID, ANIME THUMBNAIL, SUB OR DUB (sub=0, dub=1]]
     val favoriteState by remember { mutableStateOf(false) }
     val title = anime[0].replace("\"", "")
@@ -91,7 +180,7 @@ fun AnimeMostPopularDetailedCardItem(anime: List<String>) {
 
     var genreItemsState by remember { mutableStateOf(listOf("")) }
 
-    LaunchedEffect(true) {
+    LaunchedEffect(Unit) {
         getGenreItemsList(id)
         genreItemsState = genreItems
     }
@@ -102,9 +191,16 @@ fun AnimeMostPopularDetailedCardItem(anime: List<String>) {
 
     Row(
         modifier = Modifier
+            .clickable(
+                enabled = true,
+                interactionSource = remember { MutableInteractionSource() },
+                indication = rememberRipple(color = MaterialTheme.colorScheme.secondary),
+                onClick = { navController.navigate("${AnimeDetailsScreenNav.AnimeDetails.route}/$id") }
+            )
             .fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically
     ) {
+        // thumbnail
         Card(
             modifier = Modifier
                 .height(240.dp)
@@ -138,6 +234,7 @@ fun AnimeMostPopularDetailedCardItem(anime: List<String>) {
             }
         }
 
+        // text title, genre, favorite button
         Column(
             modifier = Modifier
                 .padding(16.dp, 0.dp, 0.dp, 0.dp)
@@ -170,7 +267,7 @@ fun AnimeMostPopularDetailedCardItem(anime: List<String>) {
                     onClick = { },
                     colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.onSecondaryContainer),
                     modifier = Modifier
-                        .width(130.dp),
+                        .width(135.dp),
                     content = {
                         Row(
                             horizontalArrangement = Arrangement.spacedBy(8.dp),
@@ -227,7 +324,7 @@ fun fillGenreItemsList(animeID: String) {
 
 var mostPopularItems = listOf(listOf(""))
 suspend fun getMostPopularDetailedList(sort: String = "popular-week", page: String = "1") = withContext(Dispatchers.IO) {
-    fillMostPopularDetailedList(sort = sort, page = "1")
+    fillMostPopularDetailedList(sort = sort, page = page)
 }
 fun fillMostPopularDetailedList(sort: String = "popular-week", page: String = "1") {
     mostPopularItems = AnimeSearch.getSearchResultList(sort = sort, dub = "", page = page)
